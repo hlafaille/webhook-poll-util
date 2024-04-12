b3b4523670c3e242daa26c3ff65f7bade8c32283
hlafaille <hlafaille@proton.me>
2024-04-12 13:52:14 +0000

    removing pyc

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from handler.introspect import get_handler_paths
 from util import log
 
 
-def main():
+def main() -> int:
     """Entry point"""
     l = log.get_logger("main")
     l.info("starting up")
@@ -14,12 +14,13 @@ def main():
     for path in handler_paths:
         configured_handlers.append(ConfiguredHandler.build(path))
     
-    # enter the server loop
-    while True:
+    # enter the server loop, only exiting if exit_code becomes an int
+    exit_code: int | None = None
+    while exit_code == None:
         # poll the handlers that need polling
         for handler in [x for x in configured_handlers if x.needs_polling()]:
             handler.poll()
-
+    return exit_code
 
 if __name__ == "__main__":
-    main()
+    exit(main())
